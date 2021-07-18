31899bd21c99043939426c56fc2aa5b2676cc345
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-07-18 17:28:42 -0300

    Started project. DevTools, Lombok, SpringWeb, SpringDataJPA, Actuator, H2 Database dependencies

---
+++ src/test/java/one/digitalinnovation/peoplemanager/PeoplemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.peoplemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
